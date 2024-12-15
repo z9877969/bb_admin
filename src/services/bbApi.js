d9fc0ce97e2229e610b9be7c292ac38f70430ce2
z9877969 <z9877969@ukr.net>
2024-12-15 22:37:39 +0200

    fixes baseUrl

--- src/services/bbApi.js
+++ src/services/bbApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const bbInstance = axios.create({
-  baseURL: 'http://localhost:4040/api',
+  // baseURL: 'http://localhost:4040/api',
   // baseURL: 'https://api.brushbuddy.com.ua/api',
-  // baseURL: 'https://brush-buddy-back.onren/der.com/api',
+  baseURL: 'https://brush-buddy-back.onrender.com/api',
 });
